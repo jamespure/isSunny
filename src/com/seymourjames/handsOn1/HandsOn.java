856c82265f2984f8c244c4ca68cb277502f8f190
seymour <seymourpjames@gmail.com>
2022-03-07 14:22:14 -0700

    learning java var and types

---
+++ src/com/seymourjames/handsOn1/HandsOn.java
@@ -0,0 +1,24 @@
+package com.seymourjames.handsOn1;
+
+public class HandsOn {
+
+	public static void main(String[] args) {
+		boolean isSunny = false;
+		boolean atBeach = true;
+		
+		if (isSunny == true) {
+			System.out.println("Wear sunglasses!");
+			
+			if (atBeach == true) {
+				System.out.println("Wear sunblock!");
+				System.out.println("Come back tomorrow.");
+			} else {
+				System.out.println("Don't need sunblock.");
+			}
+		} else {
+			System.out.println("Don't wear sunglasses.");
+			System.out.println("Don't go to the beach.");
+		}
+	}
+
+}
