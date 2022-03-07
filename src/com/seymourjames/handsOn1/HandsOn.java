99c472700e9072922b69baef2359d23f405e37c1
seymour <seymourpjames@gmail.com>
2022-03-07 15:31:35 -0700

    logic short hand

--- src/com/seymourjames/handsOn1/HandsOn.java
+++ src/com/seymourjames/handsOn1/HandsOn.java
@@ -4,12 +4,12 @@ public class HandsOn {
 
 	public static void main(String[] args) {
 		boolean isSunny = true;
-		boolean atBeach = true;
+		boolean atBeach = false;
 		
-		if (isSunny == true) {
+		if (isSunny) {
 			System.out.println("Wear sunglasses!");
 			
-			if (atBeach == true) {
+			if (atBeach) {
 				System.out.println("Wear sunblock!");
 				System.out.println("Come back tomorrow.");
 			} else {
@@ -19,6 +19,8 @@ public class HandsOn {
 			System.out.println("Don't wear sunglasses.");
 			System.out.println("Don't go to the beach.");
 		}
+		
+		
 	}
 
 }
